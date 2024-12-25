1408cbc2dc38e74a012883e2108a14ae230d7a8f
prathapmegavath <prathapsinghmegavath@gmail.com>
2024-12-25 13:33:01 +0530

    dom part 1 task completed

---
+++ dom-part-1/index.js
@@ -0,0 +1,40 @@
+let h1tag=document.getElementById("h1");
+console.log(h1tag)
+h1tag.style.backgroundColor="red"
+h1tag.style.padding="60px"
+h1tag.style.cursor="pointer"
+h1tag.style.borderRadius="20px "
+h1tag.style.boxshadow="10px 20px 25px 10px blue"
+h1tag.onclick=h1tag.textContent="hello parthu"
+// h1tag.style.dblclick=h1tag.textContent="your are doubled clicked"
+// function changeText(){
+//     h1.textContent="never be undeeer anyone"
+// }
+// changeText()
+
+let h2tag=document.getElementById("h2");
+console.log(h2tag)
+h2tag.style.backgroundColor="red"
+h2tag.style.padding="60px"
+h2tag.style.cursor="pointer"
+h2tag.style.borderRadius="20px "
+h2tag.style.boxshadow="10px 20px 25px 10px blue"
+h2tag.style.dblclick=h2tag.textContent="your are doubled clicked"
+// function changeText1(){
+//     h2tag.textContent="construct the flow of learning"
+// }
+// changeText1()
+
+let h3tag=document.getElementById("h3");
+console.log(h3tag)
+h3tag.style.backgroundColor="red"
+h3tag.style.padding="60px"
+h3tag.style.cursor="pointer"
+h3tag.style.borderRadius="20px "
+h3tag.style.boxshadow="10px 20px 25px 10px blue"
+// h3tag.style.onclick=h3tag.textContent="hello parthu"
+// h3tag.style.dblclick=h3tag.textContent="your are doubled clicked"
+function changeText2(){
+    h3tag.textContent="your mine at  last"
+}
+changeText2()
